3d5850b4cfab0046052a11dd93f26e144f6b8436
Kyndt Clovis <clovis.kyndt@epitech.eu>
2017-08-26 23:40:37 +0200

    finish step 1

--- src/index.js
+++ src/index.js
@@ -3,18 +3,38 @@ import html from './feedly.html'
 
 const feedback = () => {
   document.write(html)
+
+}
+
+/* function */
+window.fdlyTextarea = (value) => {
+  if (value && value.length > 0) {
+    document.getElementsByClassName("feedback_btn")[0].removeAttribute("disabled")
+  } else {
+    document.getElementsByClassName("feedback_btn")[0].setAttribute("disabled", '')
+  }
 }
 
 window.fdlyOpen = () => {
-  document.getElementById('feedback-wrapper').setAttribute("data-v-state", "open");
+  document.getElementById('feedback-wrapper').setAttribute("data-v-state", "open")
 }
 
 window.fdlyClose = () => {
-  document.getElementById('feedback-wrapper').setAttribute("data-v-state", "close");
+  let el = document.getElementById('feedback-wrapper');
+  el.setAttribute("data-v-state", "close");
+  setTimeout(function () {
+    el.removeAttribute("data-emoticon");
+    document.getElementsByClassName("feedback_textarea")[0].value = '';
+    document.getElementsByClassName("feedback_btn")[0].setAttribute("disabled", '');
+  }, 200);
 }
 
 window.fdlySend = () => {
   alert("send")
+}
+
+window.fdlyEmoticon = (id) => {
+  document.getElementById('feedback-wrapper').setAttribute('data-emoticon', id);
 }
 
 feedback()
