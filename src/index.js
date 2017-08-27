68ca5eb6ee713645bd60145bed8aa63a0f8c14d9
Kyndt Clovis <clovis.kyndt@epitech.eu>
2017-08-27 17:40:18 +0200

    add emoji

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import './style.scss'
 import html from './feedly.html'
 
-let els = {}
+let els
 
 const feedback = () => {
   document.write(html)
   els = {
     "send": document.getElementById("feedback_send"),
     "wrapper": document.getElementById("feedback-wrapper"),
-    "comment": document.getElementById("feedback_comment")
+    "comment": document.getElementById("feedback_comment"),
+    "email": document.getElementById("feedback_email")
   }
 }
 
@@ -37,9 +38,10 @@ window.fdlyOpen = () => {
 window.fdlyClose = () => {
   els.wrapper.setAttribute("data-v-state", "close");
 
-  setTimeout(function () {
+  setTimeout(() => {
     els.wrapper.removeAttribute("data-emoticon");
     els.comment.value = '';
+    els.email.value = '';
     els.send.setAttribute("disabled", "")
   }, 200);
 }
@@ -48,6 +50,14 @@ window.fdlySend = () => {
   if (els.wrapper.getAttribute('data-state') === "1") {
     els.wrapper.setAttribute("data-state", 2)
     els.send.setAttribute("disabled", "")
+  } else {
+    window.fdlyClose()
+    setTimeout(() => {els.wrapper.setAttribute("data-state", 3)}, 150);
+    setTimeout(() => {
+      if (els.wrapper.getAttribute('data-state') === "3") {
+        els.wrapper.setAttribute("data-state", 1)
+      }
+    }, 4000);
   }
 }
 
